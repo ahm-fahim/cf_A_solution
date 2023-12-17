f05b4edcf687d2d8414330127ffb15a900ed5ed3
ahm-fahim <contactahmfahim@gmail.com>
2023-12-17 20:08:11 +0600

    solved

--- 5.cpp
+++ 5.cpp
@@ -33,10 +33,18 @@
 using namespace std;
 #define optimize() ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
 #define endl '\n';
-
+#define ll long long int
 int main()
 {
     optimize();
+    ll t;
+    cin >> t;
+
+    while(t--){
+        ll n;
+        cin >> n;
+        cout << n / 2 << endl;
+    }
 
     return 0;
 }
